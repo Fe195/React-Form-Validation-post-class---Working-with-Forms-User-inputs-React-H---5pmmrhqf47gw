7d143afee28bbdb5350ea51e622c5446b11526a9
Fe195 <114395573+Fe195@users.noreply.github.com>
2022-12-08 21:37:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,68 +1,110 @@
-
 import React, { useState, useRef } from 'react';
+
 /**
- * @task :add validation to email, if email is not valid, if not valid email, dont allow to submit
- * @error_message :  "Email is invalid"  if email is wrong. (must be same message) 
- * 
- * 
- */
+* @task :add validation to email, if email is not valid
+* @error message : show "Email is invalid" message on wrong email.
+*/
 
 function App() {
 
- /**
-  * code here
-  */
-   const fnameRef = useRef();
-  const emailRef = useRef();
-  const [error, setError] = useState(undefined);
-  const [data, setData] = useState({ fname: undefined, lname: undefined });
+const fnameRef= useRef("");
 
-  const change = () => {
-    // console.log(emailRef.current.value);
-    if (/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(emailRef.current.value)) {
-      setError(undefined);
-      document.getElementById("submit").disabled = false;
-    } else {
-      setError("Email is invalid");
-      document.getElementById("submit").disabled = true;
-    }
-  };
-        
+const emailRef = useRef("");
 
-  return(
-    <div className="App">
-      <h1>How About Them Apples</h1>
-      <form onSubmit={(e) => {
-          e.preventDefault();
-          setData({
-            fname: fnameRef.current.value,
-            lname: emailRef.current.value
-          });
-        }}>
-        <fieldset>
-          <label>
-            <p>First Name</p>
-            <input id='fname' name="name"  ref={fnameRef}/>
-            <br></br>
-            <p>Email</p>
-            <input id='lname' name="name" onChange={change}  ref={emailRef}/>
-            {error && <h2 style={{color: 'red'}}>{error}</h2>}
-          </label>
-        </fieldset>
+const [data, setdata] = useState({});
 
-        <button id='submit' type="submit">Submit</button>
-      </form>
-      {
-        data.fname != undefined && (
-          <div>
-          <h1>{data.fname}</h1>
-          <h2>{data.lname}</h2>
-          </div>
-        )
-      }
-    </div>
-  )
+const handleSubmit = event => {
+
+event.preventDefault();
+
+if(error === 'Email is invalid') return ;
+
+const data = {"fname" : fnameRef.current.value, "email": emailRef.current.value};
+
+console.log(data)
+
+setdata((pre)=> data);
+
 }
 
+const [message, setMessage] = useState('');
+
+const [error, setError] = useState(null);
+
+function isValidEmail(email) {
+
+return /\S+@\S+\.\S+/.test(email);
+
+}
+
+const handleChange = event => {
+
+if (!isValidEmail(event.target.value)) {
+
+setError('Email is invalid');
+
+} else {
+
+setError(null);
+
+}
+
+setMessage(event.target.value);
+
+};
+
+return(
+
+<div className="App">
+
+<h1>How About Them Apples</h1>
+
+<form onSubmit={handleSubmit}>
+
+<fieldset>
+
+<label>
+
+<p>First Name</p>
+
+<input id='fname' name="name" ref={fnameRef}/>
+
+<br></br>
+
+<p>Email</p>
+
+<input id='lname' name="name" onChange={handleChange} ref={emailRef}/>
+
+{error && <h2 style={{color: 'red'}}>{error}</h2>}
+
+</label>
+
+</fieldset>
+
+<button id='submit' type="submit">Submit</button>
+
+</form>
+
+{
+
+data.fname != undefined && (
+
+<div>
+
+<h1>{data.fname}</h1>
+
+<h2>{data.lname}</h2>
+
+</div>
+
+)
+
+}
+
+</div>
+
+)
+
+}
 
 export default App;
